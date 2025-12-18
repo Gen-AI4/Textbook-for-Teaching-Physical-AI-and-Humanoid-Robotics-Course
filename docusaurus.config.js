6d4cc28555dbd2546fea4711eda279b7bf2d1e7b
Gen-AI4 <Gen-AI4@users.noreply.github.com>
2025-12-18 11:49:08 +0500

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://gen-ai4.github.io',
+  url: 'https://textbook-for-teaching-physical-ai-a-ten.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Textbook-for-Teaching-Physical-AI-and-Humanoid-Robotics-Course/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
